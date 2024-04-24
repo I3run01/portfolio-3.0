337e44b314cc5f0745286e6bee39c3c69d533034
I3run01 <brunnooa.v@gmail.com>
2024-04-24 09:32:22 -0300

    working with mobile

--- src/components/mobileTopMenu/index.tsx
+++ src/components/mobileTopMenu/index.tsx
@@ -1,10 +1,24 @@
 import { MobileTopMenuDiv } from "./mobileTopMenu.style"
+import { BurguerMenu } from "../leftMenuBar/BurgerMenu"
+import { SvgIcons } from "src/assets/svg/svgIcons"
+import { RootState } from "src/redux/store"
+import { useSelector } from "react-redux"
+import { Colors } from "src/styles/globalVariables.style"
 
-export const MobileTopMenu = () => {
+type pros = {
+    burguerFunction: () => void,
+    homeFunction: () => void,
+    shareFunction: () => void,
+}
+
+export const MobileTopMenu = ({burguerFunction, homeFunction, shareFunction}: pros) => {
+    const isDark = useSelector((state: RootState) => state.theme.isDark) 
 
     return (
-        <MobileTopMenuDiv>
-
+        <MobileTopMenuDiv $svgColor={isDark ? Colors.darkFontColor : Colors.lightFontColor}>
+            <BurguerMenu fction={burguerFunction} isMenuClosed/>
+            <div className="icons" onClick={homeFunction}><SvgIcons.HomeIcon /></div>
+            <div className="icons" onClick={shareFunction}><SvgIcons.ShareIcon /></div>           
         </MobileTopMenuDiv>
     )
 }
--- src/components/mobileTopMenu/mobileTopMenu.style.tsx
+++ src/components/mobileTopMenu/mobileTopMenu.style.tsx
@@ -1,15 +1,41 @@
 import styled from "styled-components";
+import { Colors } from "src/styles/globalVariables.style";
 
-export const MobileTopMenuDiv = styled.div`
+type props = {
+    $svgColor: string
+}
+
+export const MobileTopMenuDiv = styled.div<props>`
     display: none;
     
     height: 40px;
     width: 100%;
 
-    background-color: red;
+    background: rgba(15, 0, 237, 0.24);
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(7.4px);
+    -webkit-backdrop-filter: blur(7.4px);
+
+    .icons {
+        display: flex;
+        align-items: center;
+        height: 100%;
+        cursor: pointer;
+
+        :hover {
+            color: ${Colors.themeColor01};
+        }
+
+        svg {
+            color: ${props => props.$svgColor};
+            height: 80%;
+        }
+    }
 
     @media screen and (max-width: 800px) {
-        display: block;
+        display: flex;
+        justify-content: space-around;
+
     }
 
 `
